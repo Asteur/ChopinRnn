40e0bb37abe422e1e49a6ca9bb6e190158ff224a
Yizong98 <yizong@ucsd.edu>
2018-05-17 19:33:14 -0700

    chore: Import necessary packages for training model

---
+++ chopin_model_train.py
@@ -0,0 +1,11 @@
+import glob
+import pickle
+import numpy
+from music21 import converter, instrument, note, chord
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.layers import Activation
+from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint
